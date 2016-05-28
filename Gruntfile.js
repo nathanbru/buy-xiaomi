0188e61dd54ee7ac5573ef1772b39e5acacb84c7
nathan14 <nathan.bru@gmail.com>
2016-05-28 12:32:59 +0300

    More grunt options

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'src/**/*.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
     },
+    less: {
+      files: {
+        'dist/css/style.css': 'src/css/style.less'
+      }
+    },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      files: ['src/**/*'],
+      tasks: ['jshint', 'less']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.registerTask('default', ['jshint']);
 };
